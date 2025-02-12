ae2884c2fc22da863af1e09c635683b7dac1dfa8
Brian Salchert <brisalchert@gmail.com>
2025-02-12 15:29:23 -0500

    Add calculator buttons to layout

--- Module1/Calculator/app/src/main/java/com/example/calculator/FirstFragment.kt
+++ Module1/Calculator/app/src/main/java/com/example/calculator/FirstFragment.kt
@@ -31,9 +31,6 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-        }
     }
 
     override fun onDestroyView() {
