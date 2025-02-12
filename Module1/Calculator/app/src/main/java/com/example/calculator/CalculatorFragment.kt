3b9b59360e09c0414de2b4bec5f1a31298c7dced
Brian Salchert <brisalchert@gmail.com>
2025-02-12 17:27:39 -0500

    Remove redundant code + clean up warnings

--- Module1/Calculator/app/src/main/java/com/example/calculator/CalculatorFragment.kt
+++ Module1/Calculator/app/src/main/java/com/example/calculator/CalculatorFragment.kt
@@ -5,15 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.calculator.databinding.FragmentFirstBinding
+import com.example.calculator.databinding.FragmentCalculatorBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class CalculatorFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentCalculatorBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,13 +23,9 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentCalculatorBinding.inflate(inflater, container, false)
         return binding.root
 
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onDestroyView() {
