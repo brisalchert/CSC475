904a44855cd4ee6c721780efce9381ae4bf7cf0a
Brian Salchert <brisalchert@gmail.com>
2025-03-17 14:27:23 -0400

    Update dependency versions

--- Module5/PhotoGallery/app/build.gradle.kts
+++ Module5/PhotoGallery/app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.androidx.lifecycle.runtime.ktx.v262)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     // Network request and image libraries
