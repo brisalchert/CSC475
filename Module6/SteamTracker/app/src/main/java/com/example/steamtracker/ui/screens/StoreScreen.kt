ddc23980d66a979c6d15acaedd5c02161cdeb800
Brian Salchert <brisalchert@gmail.com>
2025-03-17 21:00:32 -0400

    Add store search bar

--- Module6/SteamTracker/app/src/main/java/com/example/steamtracker/ui/screens/StoreScreen.kt
+++ Module6/SteamTracker/app/src/main/java/com/example/steamtracker/ui/screens/StoreScreen.kt
@@ -1,28 +1,62 @@
 package com.example.steamtracker.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.preferKeepClear
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.focus.focusProperties
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.example.steamtracker.R
 import com.example.steamtracker.ui.theme.SteamTrackerTheme
 
@@ -43,6 +77,14 @@ fun StoreScreen(
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun StoreScreenPreview() {
+    SteamTrackerTheme {
+        StoreScreen(TrackerUiState.Success, {})
+    }
+}
+
 @Composable
 fun LoadingScreen(modifier: Modifier = Modifier) {
     Image(
@@ -50,6 +92,14 @@ fun LoadingScreen(modifier: Modifier = Modifier) {
         painter = painterResource(R.drawable.loading_img),
         contentDescription = stringResource(R.string.loading)
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingScreenPreview() {
+    SteamTrackerTheme {
+        LoadingScreen()
+    }
 }
 
 @Composable
@@ -85,23 +135,106 @@ fun FeaturedGameList(
     var tabIndex by remember { mutableIntStateOf(0) }
     val tabs = listOf("Featured", "On Sale", "Recommended")
 
-    Column(
+    Box(
         modifier = modifier
+            .fillMaxSize()
     ) {
-        TabRow(
-            selectedTabIndex = tabIndex
+        StoreSearchBar()
+
+        Column(
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            tabs.forEachIndexed { index, title ->
-                Tab(
-                    text = { Text(title) },
-                    selected = tabIndex == index,
-                    onClick = { tabIndex = index }
-                )
+            Spacer(modifier = modifier.height(76.dp))
+
+            TabRow(
+                selectedTabIndex = tabIndex
+            ) {
+                tabs.forEachIndexed { index, title ->
+                    Tab(
+                        text = { Text(title) },
+                        selected = tabIndex == index,
+                        onClick = { tabIndex = index }
+                    )
+                }
+            }
+
+            when(tabIndex) {
+                // TODO: Implement screens for each tab
             }
         }
+    }
+}
 
-        when(tabIndex) {
+@Preview(showBackground = true)
+@Composable
+fun FeaturedGameListPreview() {
+    SteamTrackerTheme {
+        FeaturedGameList()
+    }
+}
 
+@Composable
+fun StoreSearchBar(
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+    Card(
+        modifier = modifier
+            .wrapContentSize()
+            .padding(10.dp)
+            .zIndex(1f),
+        colors = CardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            disabledContainerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            disabledContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        val query = remember { mutableStateOf("") }
+
+        TextField(
+            value = query.value,
+            onValueChange = { query.value = it },
+            placeholder = { Text("Search the Steam Store") },
+            singleLine = true,
+            leadingIcon = {
+                Icon(
+                    Icons.Filled.Menu,
+                    contentDescription = "Menu Icon",
+                    modifier = modifier.padding(PaddingValues(10.dp, 0.dp, 0.dp, 0.dp))
+                )
+            },
+            trailingIcon = {
+                if (query.value.isNotEmpty()) {
+                    IconButton(onClick = { query.value = "" }) {
+                        Icon(Icons.Filled.Close, contentDescription = "Clear Query")
+                    }
+                } else {
+                    Icon(Icons.Filled.Search, contentDescription = null)
+                }
+            },
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                unfocusedContainerColor = MaterialTheme.colorScheme.primaryContainer
+            ),
+            modifier = modifier
+                .fillMaxWidth()
+        )
+
+        // TODO: Add list of autocomplete results
+        if (query.value.isNotEmpty()) {
+            Column(modifier = modifier.height(200.dp)) {}
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StoreSearchBarPreview() {
+    SteamTrackerTheme {
+        StoreSearchBar()
     }
 }
