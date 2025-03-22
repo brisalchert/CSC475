63f23689d5bd8cf06dadfa600ab8194c11a342f9
Brian Salchert <brisalchert@gmail.com>
2025-03-21 21:20:42 -0400

    Create data classes for SteamSpy requests

---
+++ Module6/SteamTracker/app/src/main/java/com/example/steamtracker/model/SteamSpyAppRequest.kt
@@ -0,0 +1,31 @@
+package com.example.steamtracker.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SteamSpyAppRequest(
+    val appid: Int,
+    val name: String,
+    val developer: String,
+    val publisher: String,
+    @SerializedName(value = "score_rank")
+    val scoreRank: String,
+    val positive: Int,
+    val negative: Int,
+    val userscore: Int,
+    val owners: String,
+    @SerializedName(value = "average_forever")
+    val averageForever: Int,
+    @SerializedName(value = "average_2weeks")
+    val average2Weeks: Int,
+    @SerializedName(value = "median_forever")
+    val medianForever: Int,
+    @SerializedName(value = "median_2weeks")
+    val median2Weeks: Int,
+    val price: String,
+    val initialprice: String,
+    val discount: String,
+    val ccu: Int,
+    val languages: String,
+    val genre: String,
+    val tags: Map<String, Integer>
+)
