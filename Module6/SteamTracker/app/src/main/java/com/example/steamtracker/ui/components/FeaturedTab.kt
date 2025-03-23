fb3baf42e06dc346f9cf66d677fff6c563f50769
Brian Salchert <brisalchert@gmail.com>
2025-03-22 23:13:16 -0400

    Update featured UI with Category headers

--- Module6/SteamTracker/app/src/main/java/com/example/steamtracker/ui/components/FeaturedTab.kt
+++ Module6/SteamTracker/app/src/main/java/com/example/steamtracker/ui/components/FeaturedTab.kt
@@ -1,6 +1,10 @@
 package com.example.steamtracker.ui.components
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -8,15 +12,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -29,7 +36,6 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.steamtracker.R
 import com.example.steamtracker.model.AppInfo
-import com.example.steamtracker.model.ContentDescriptors
 import com.example.steamtracker.model.FeaturedCategoriesRequest
 import com.example.steamtracker.ui.screens.LoadingScreen
 import com.example.steamtracker.ui.screens.StoreErrorScreen
@@ -98,6 +104,7 @@ fun FeaturedGamesList(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun FeaturedCategoriesList(
     featuredCategories: FeaturedCategoriesRequest,
@@ -110,17 +117,53 @@ fun FeaturedCategoriesList(
         horizontalAlignment = Alignment.Start
     ) {
         featuredCategories.specials?.let {
-            item {
-                Text(text = "Specials", fontSize = 24.sp)
+            stickyHeader {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(64.dp)
+                        .shadow(8.dp, RoundedCornerShape(0.dp))
+                        .background(MaterialTheme.colorScheme.primaryContainer),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "SPECIALS",
+                        fontSize = 30.sp,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
             item {
                 FeaturedGamesList(featuredCategories.specials.items!!)
             }
         }
 
+        item {
+            Spacer(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+            )
+        }
+
         featuredCategories.topSellers?.let {
-            item {
-                Text(text = "Top Sellers", fontSize = 24.sp)
+            stickyHeader {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(64.dp)
+                        .shadow(8.dp, RoundedCornerShape(0.dp))
+                        .background(MaterialTheme.colorScheme.primaryContainer),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "TOP SELLERS",
+                        fontSize = 30.sp,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
             item {
                 FeaturedGamesList(featuredCategories.topSellers.items!!)
