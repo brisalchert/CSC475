53c544802a22818ce61afa67c98bef793ed9926d
Brian Salchert <brisalchert@gmail.com>
2025-04-04 13:01:07 -0400

    Add CollectionsViewModelTest

---
+++ Module6/SteamTracker/app/src/test/java/com/example/steamtracker/CollectionsViewModelTest.kt
@@ -0,0 +1,58 @@
+package com.example.steamtracker
+
+import android.app.Application
+import com.example.steamtracker.fake.FakeCollectionApp
+import com.example.steamtracker.fake.FakeNetworkCollectionsRepository
+import com.example.steamtracker.fake.FakeNetworkStoreRepository
+import com.example.steamtracker.room.entities.CollectionAppEntity
+import com.example.steamtracker.room.entities.CollectionEntity
+import com.example.steamtracker.room.relations.CollectionWithApps
+import com.example.steamtracker.rules.TestDispatcherRule
+import com.example.steamtracker.ui.screens.CollectionsViewModel
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.spy
+
+class CollectionsViewModelTest {
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
+
+    private lateinit var mockApplication: Application
+    private lateinit var collectionsViewModel: CollectionsViewModel
+
+    @Before
+    fun setup() {
+        mockApplication = Mockito.mock(Application::class.java)
+
+        collectionsViewModel = CollectionsViewModel(
+            storeRepository = FakeNetworkStoreRepository(),
+            collectionsRepository = FakeNetworkCollectionsRepository(),
+            workManager = null
+        )
+    }
+
+    @Test
+    fun collectionsViewModel_getAllCollections_verifyCollectionsUiStateSuccess() =
+        runTest {
+            // View model calls getAllCollections() on init
+            assertEquals(
+                listOf(
+                    CollectionWithApps(
+                        collection = CollectionEntity(FakeCollectionApp.response.collectionName),
+                        collectionAppsDetails = listOf(CollectionAppEntity(
+                            collectionName = FakeCollectionApp.response.collectionName,
+                            appid = FakeCollectionApp.response.appId,
+                            index = FakeCollectionApp.response.index
+                        ))
+                    )
+                ),
+                collectionsViewModel.allCollections.first()
+            )
+        }
+}
