c067ea8e25c352771922ff0f4b1ad14883cb944b
Brian Salchert <brisalchert@gmail.com>
2025-04-04 12:01:32 -0400

    Add SearchViewModelTest

---
+++ Module6/SteamTracker/app/src/test/java/com/example/steamtracker/SearchViewModelTest.kt
@@ -0,0 +1,54 @@
+package com.example.steamtracker
+
+import com.example.steamtracker.fake.FakeNetworkStoreRepository
+import com.example.steamtracker.fake.FakeStoreSearchRequest
+import com.example.steamtracker.rules.TestDispatcherRule
+import com.example.steamtracker.ui.components.SearchUiState
+import com.example.steamtracker.ui.components.SearchViewModel
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class SearchViewModelTest {
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
+
+    private lateinit var searchViewModel: SearchViewModel
+
+    @Before
+    fun setup() {
+        searchViewModel = SearchViewModel(
+            storeRepository = FakeNetworkStoreRepository()
+        )
+    }
+
+    @Test
+    fun searchViewModel_getAutocompleteResults_verifyAutocompleteResultsCorrect() =
+        runTest {
+            searchViewModel.getAutocompleteResults("")
+
+            assertEquals(
+                FakeStoreSearchRequest.response,
+                searchViewModel.autocompleteResults.first()
+            )
+        }
+
+    @Test
+    fun searchViewModel_getSearchResults_verifySearchUiStateSuccess() =
+        runTest {
+            searchViewModel.getSearchResults("")
+
+            assertEquals(
+                SearchUiState.Success(FakeStoreSearchRequest.response.items),
+                searchViewModel.searchUiState.first()
+            )
+
+            assertEquals(
+                FakeStoreSearchRequest.response,
+                searchViewModel.searchResults.first()
+            )
+        }
+}
