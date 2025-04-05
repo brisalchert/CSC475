1f55c77587e92c13e5e27457e799dcdde43938f5
Brian Salchert <brisalchert@gmail.com>
2025-04-05 17:24:32 -0400

    Add tests for App Page collections boxes

---
+++ Module6/SteamTracker/app/src/androidTest/java/com/example/steamtracker/ui/FavoritesBoxTest.kt
@@ -0,0 +1,59 @@
+package com.example.steamtracker.ui
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollToIndex
+import com.example.steamtracker.ui.theme.SteamTrackerTheme
+import org.junit.Rule
+import org.junit.Test
+
+class FavoritesBoxTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun recommendedGame_verifyAppPageShows() {
+        composeTestRule.setContent {
+            SteamTrackerTheme {
+                SteamTrackerApp()
+            }
+        }
+
+        // Wait for the Recommended tab to load
+        composeTestRule.waitUntilExactlyOneExists(
+            hasTestTag("FeaturedGamesList")
+        )
+
+        composeTestRule.onNodeWithTag("FeaturedGamesList")
+            .performScrollToIndex(1)
+            .performClick()
+
+        // Verify App Page text exists
+        composeTestRule.waitUntilExactlyOneExists(
+            hasText("Add to Favorites")
+        )
+
+        // Add game to favorites
+        composeTestRule.onNodeWithText("Add to Favorites")
+            .performClick()
+
+        // Verify Added status
+        composeTestRule.onNodeWithContentDescription("Added to Favorites")
+            .assertExists()
+
+        // Remove game from favorites
+        composeTestRule.onNodeWithText("Add to Favorites")
+            .performClick()
+
+        // Verify Removed status
+        composeTestRule.onNodeWithContentDescription("Not on Favorites")
+            .assertExists()
+    }
+}
---
+++ Module6/SteamTracker/app/src/androidTest/java/com/example/steamtracker/ui/NewsListBoxTest.kt
@@ -0,0 +1,59 @@
+package com.example.steamtracker.ui
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollToIndex
+import com.example.steamtracker.ui.theme.SteamTrackerTheme
+import org.junit.Rule
+import org.junit.Test
+
+class NewsListBoxTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun recommendedGame_verifyAppPageShows() {
+        composeTestRule.setContent {
+            SteamTrackerTheme {
+                SteamTrackerApp()
+            }
+        }
+
+        // Wait for the Recommended tab to load
+        composeTestRule.waitUntilExactlyOneExists(
+            hasTestTag("FeaturedGamesList")
+        )
+
+        composeTestRule.onNodeWithTag("FeaturedGamesList")
+            .performScrollToIndex(1)
+            .performClick()
+
+        // Verify App Page text exists
+        composeTestRule.waitUntilExactlyOneExists(
+            hasText("Track News")
+        )
+
+        // Add game to wishlist
+        composeTestRule.onNodeWithText("Track News")
+            .performClick()
+
+        // Verify Added status
+        composeTestRule.onNodeWithContentDescription("Added to News list")
+            .assertExists()
+
+        // Remove game from wishlist
+        composeTestRule.onNodeWithText("Track News")
+            .performClick()
+
+        // Verify Removed status
+        composeTestRule.onNodeWithContentDescription("Not on News list")
+            .assertExists()
+    }
+}
