1435328f1d06a27e4dcfa3015ee7b74c4b5785f4
Brian Salchert <brisalchert@gmail.com>
2025-04-05 20:14:03 -0400

    Add preferences test

--- Module6/SteamTracker/app/src/androidTest/java/com/example/steamtracker/ui/MenuScreenTests.kt
+++ Module6/SteamTracker/app/src/androidTest/java/com/example/steamtracker/ui/MenuScreenTests.kt
@@ -2,19 +2,17 @@ package com.example.steamtracker.ui
 
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.assertContentDescriptionEquals
-import androidx.compose.ui.test.assertTextContains
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onChild
 import androidx.compose.ui.test.onChildAt
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performKeyInput
+import androidx.compose.ui.test.performScrollToNode
 import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.pressKey
 import com.example.steamtracker.ui.theme.SteamTrackerTheme
@@ -109,5 +107,79 @@ class MenuScreenTests {
         composeTestRule.waitUntilExactlyOneExists(
             hasText("Search the Steam Store")
         )
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun menuPreferences_verifyPreferencesAddedRemoved() {
+        // Switch to Search screen
+        composeTestRule.onNodeWithTag("MenuList")
+            .onChildAt(4)
+            .assertTextEquals("Search")
+            .performClick()
+        composeTestRule.waitUntilExactlyOneExists(
+            hasText("Search the Steam Store")
+        )
+
+        // Search for game
+        composeTestRule.onNodeWithText("Search the Steam Store")
+            .performClick()
+            .performTextInput("Elden Ring")
+        composeTestRule.onNodeWithText("Elden Ring")
+            .performKeyInput {
+                pressKey(Key.Enter)
+            }
+
+        // Wait for search results
+        composeTestRule.waitUntilExactlyOneExists(
+            hasTestTag("SearchResultsList")
+        )
+
+        // Select game
+        composeTestRule.onNodeWithText("ELDEN RING")
+            .performClick()
+
+        // Wait for app screen
+        composeTestRule.waitUntilExactlyOneExists(
+            hasTestTag("AppPageList")
+        )
+
+        // Add a genre and a tag
+        composeTestRule.onNodeWithTag("AppPageList")
+            .performScrollToNode(
+                hasTestTag("TagsList")
+            )
+        composeTestRule.onNodeWithTag("GenresList")
+            .onChildAt(0)
+            .performClick()
+        composeTestRule.onNodeWithTag("TagsList")
+            .onChildAt(0)
+            .performClick()
+
+        // Navigate to Preferences screen
+        composeTestRule.onNodeWithContentDescription("Menu")
+            .performClick()
+        composeTestRule.onNodeWithTag("MenuList")
+            .onChildAt(5)
+            .assertTextEquals("Preferences")
+            .performClick()
+
+        // Verify genre and tag present
+        composeTestRule.onNodeWithContentDescription("Remove genre")
+            .assertExists()
+        composeTestRule.onNodeWithContentDescription("Remove tag")
+            .assertExists()
+
+        // Remove genre and tag
+        composeTestRule.onNodeWithContentDescription("Remove genre")
+            .performClick()
+        composeTestRule.onNodeWithContentDescription("Remove tag")
+            .performClick()
+
+        // Verify genre and tag removed
+        composeTestRule.onNodeWithText("Add genres to see them here!")
+            .assertExists()
+        composeTestRule.onNodeWithText("Add tags to see them here!")
+            .assertExists()
     }
 }
