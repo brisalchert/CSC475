c003b39a3f5bb501be444a6bb0a52355fe98c060
Brian Salchert <brisalchert@gmail.com>
2025-04-06 21:17:20 -0400

    Add application ID

--- Module6/SteamTracker/app/build.gradle.kts
+++ Module6/SteamTracker/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.steamtracker"
+        applicationId = "SteamTracker"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
