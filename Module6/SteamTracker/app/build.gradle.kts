112b1d0e9da7d3dea81078720629e843fcaeefc7
Brian Salchert <brisalchert@gmail.com>
2025-04-06 21:21:31 -0400

    Update ID

--- Module6/SteamTracker/app/build.gradle.kts
+++ Module6/SteamTracker/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "SteamTracker"
+        applicationId = "com.example.steamtracker"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
